46226f1755baeec890fbb4baa9c2a99fd738fab3
xma <maxiangyucarter@gmail.com>
2022-09-14 17:18:52 -0600

    Add keyListener

--- src/main/tank/TankFrame.java
+++ src/main/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -11,6 +13,8 @@ public class TankFrame extends Frame {
         setTitle("Tank War");
         setVisible(true);
 
+        this.addKeyListener(new MyKeyListener());
+
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -19,12 +23,28 @@ public class TankFrame extends Frame {
         });
     }
 
+    //you need variables to make tank move the position
     int x = 200, y = 200;
-
     @Override   //paint相当于画笔,画出坐标x,y开始- 长宽50,50的rectangle
-    public void paint(Graphics g) {
+    public void paint(Graphics g) { //会自动调用
         g.fillRect(x, y, 50, 50);
         x += 10;
         y += 10;
+    }
+
+
+
+    class MyKeyListener extends KeyAdapter {
+        @Override//effective when press key
+        public void keyPressed(KeyEvent e) {
+            System.out.println("press");
+        }
+
+        @Override//effective when release key
+        public void keyReleased(KeyEvent e) {
+            System.out.println("release");
+        }
+
+
     }
 }
