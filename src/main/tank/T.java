02bb4bee46578e2736f815b1ef34536b630a22f9
xma <maxiangyucarter@gmail.com>
2022-09-14 16:45:00 -0600

    Initial commit

---
+++ src/main/tank/T.java
@@ -0,0 +1,7 @@
+package tank;
+
+public class T {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/tank/TankFrame.java
@@ -0,0 +1,30 @@
+package tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("Tank War");
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    int x = 200, y = 200;
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+        x += 10;
+        y += 10;
+    }
+}
